f49a0c2c0488b18738e194dcdaf71d889e93540e
Vossina Maria <vossinam.gmail.com>
2023-04-13 20:11:54 +0400

    создали котиков

---
+++ cats/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Cat(models.Model):
+    class Meta:
+        db_table = 'cats'
+    name = models.CharField(max_length=15)
+    color = models.CharField(max_length=20)
+    birth_year = models.IntegerField(null=True)
